8ba38819b3c69adb6ffce980c9ebc5b495fa9239
Ammar <56699771+amarhbaneen@users.noreply.github.com>
2024-09-22 01:59:20 +0300

    Update Intro.jsx

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Intro() {
    return (
       <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-         <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Ammar Alsanah (Hbaneen)</h1>
+         <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Ammar Alsanah</h1>
          <p className="text-base md:text-xl mb-3 font-medium">Software Engineer & Web Developer</p>
          <p className="text-sm max-w-xl mb-6 font-bold">
          Accomplished full-stack software engineer with a robust background in developing innovative and efficient solutions. Proficient in both frontend and backend 
