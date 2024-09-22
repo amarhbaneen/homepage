36f8fabd3cdcd3ee5b49648837ffe78169b60df8
Ammar <56699771+amarhbaneen@users.noreply.github.com>
2024-09-22 23:07:28 +0300

    Update timeline.js
    adding new content about college

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -19,6 +19,6 @@ export default [
 		title: 'Software Engineering Student @ SCE ',
 		duration: '4 years',
 		details:
-			"Experienced software engineer specializing in backend development with C, Java, and SQL for the Amdocs Billing System. Skilled in integrating Oracle ESB, automating server configurations, and developing user interfaces with React. Proven track record in managing CI/CD pipelines, collaborating with international teams, and ensuring application reliability through rigorous testing.",
+			"Gained a strong foundation in software development, algorithms, data structures, and system design. Completed projects in web development, mobile applications, and cloud computing.Emphasized problem-solving and software architecture with hands-on experience in programming languages like JAVA , C#,C/C++ ,Python.",
 	},
 ]
